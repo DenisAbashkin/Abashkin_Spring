456b69841f1d7e5062c0358212aacb5102120fac
DenisAbashkin <35079479+DenisAbashkin@users.noreply.github.com>
2021-12-18 02:31:20 +0300

    Initial commit

---
+++ src/main/java/com/example/abashkinspring/AbashkinspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.abashkinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbashkinspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbashkinspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/abashkinspring/AbashkinspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abashkinspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbashkinspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
