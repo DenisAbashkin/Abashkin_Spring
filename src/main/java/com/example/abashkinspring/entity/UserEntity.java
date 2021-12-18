9c8d637de846ba2c21cc1ef0d6438e3aacc81947
DenisAbashkin <35079479+DenisAbashkin@users.noreply.github.com>
2021-12-18 09:54:14 +0300

    Create table TodoEntity

--- src/main/java/com/example/abashkinspring/entity/UserEntity.java
+++ src/main/java/com/example/abashkinspring/entity/UserEntity.java
@@ -1,9 +1,7 @@
 package com.example.abashkinspring.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity//Для того чтобы JPA сделать таблицу из этой сущности (entity переводится как сущность)
 public class UserEntity {
@@ -12,6 +10,9 @@ public class UserEntity {
     private Long id;
     private String username;
     private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")//Один пользователь имеет много задач.// Каскадом мы сообщаем, что при удалении пользователя нужно удалить и его задачи
+    private List<TodoEntity> todos;
 
     public UserEntity() {
     }
